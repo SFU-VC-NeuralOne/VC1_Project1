d3e9accbc7012f56185b10c728428c2ee95e5cff
Yijun Zhou <yijunzhou88@hotmail.com>
2018-09-24 22:14:29 -0700

    Amend random test

--- UnitTest.py
+++ UnitTest.py
@@ -87,15 +87,15 @@ class TestFlipAugmentation(unittest.TestCase):
         img_og = np.asarray(img, dtype=np.float32)
         h, w, c = img_og.shape[0], img_og.shape[1], img_og.shape[2]
         label = label.reshape(7, 2) - np.asarray([bounding_box[0], bounding_box[1]])
-        label = label / np.asarray([(bounding_box[2] - bounding_box[0]), (bounding_box[3] - bounding_box[1])])
+        #label = label / np.asarray([(bounding_box[2] - bounding_box[0]), (bounding_box[3] - bounding_box[1])])
         img_rescale = img_og / 255 * 2 - 1
 
         #plt.imshow(img, cmap='brg')
         plt.imshow(img.transpose(Image.FLIP_LEFT_RIGHT), cmap='brg')
         label = fl.calculate_filp(label, h)
         print(label)
-        #plt.plot(label[:, 0] * h, label[:, 1] * h, color='green', marker='o',linestyle='none', markersize=12, label='Label')
-        plt.plot(label[0, 0] * h, label[0, 1] * h, color='green', marker='o', linestyle='none', markersize=12, label='Label')
+        plt.plot(label[:, 0], label[:, 1], color='green', marker='o',linestyle='none', markersize=5, label='Label')
+        #plt.plot(label[0, 0] * h, label[0, 1] * h, color='green', marker='o', linestyle='none', markersize=12, label='Label')
         plt.show()
         self.assertEqual('foo'.upper(), 'FOO')
 
@@ -143,6 +143,7 @@ class TestRandomImage(unittest.TestCase):
         file_path = item['file_path']
         bounding_box = item['cords'][0]
         label = item['cords'][1]
+        label = label.reshape(7, 2) - np.asarray([bounding_box[0], bounding_box[1]])
 
         img = Image.open(file_path)
         img = img.crop((bounding_box[0], bounding_box[1], bounding_box[2], bounding_box[3]))  # crop to bonding box
@@ -153,7 +154,6 @@ class TestRandomImage(unittest.TestCase):
         img = img / 255 * 2 - 1
         img_tensor = torch.from_numpy(img)
         img_tensor = img_tensor.view(1, c, h, w)
-        label_tensor = torch.from_numpy(label.flatten().astype(np.double))
 
         test_net = lfw_net()
         test_net_state = torch.load(os.path.join(lfw_dataset_dir, 'lfw_net.pth'))
@@ -162,10 +162,13 @@ class TestRandomImage(unittest.TestCase):
         pred = test_net.forward(img_tensor)
 
         pred_label = pred.cpu().detach().numpy()
+        pred_label = pred_label.reshape(7,2)
 
-        plt.title(pred )
+        print('predicted: ', pred_label*h)
+        print('ground truth: ', label)
         plt.imshow((img + 1) / 2, cmap='brg')
-        plt.plot(pred_label[:, 0] * h, pred_label[:, 1] * h, color='green', marker='o', linestyle='none', markersize=12, label='Label')
+        plt.plot(pred_label[:, 0] * h, pred_label[:, 1] * h, color='green', marker='o', linestyle='none', markersize=5, label='Label')
+
 
         plt.show()
 
